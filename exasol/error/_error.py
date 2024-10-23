0b113dc97fcb95ea5603c8109825148711711332
Nicola Coretti <nicola.coretti@exasol.com>
2024-10-23 15:43:17 +0200

    Make sure ExaError does not raise (#41)

--- exasol/error/_error.py
+++ exasol/error/_error.py
@@ -1,10 +1,13 @@
 import warnings
 from dataclasses import dataclass
+from inspect import cleandoc
 from typing import Dict, Iterable, List, Mapping, Union
+from pathlib import Path
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     from exasol_error_reporting_python import exa_error
+    from exasol_error_reporting_python.error_message_builder import InvalidErrorCode
 
 
 @dataclass(frozen=True)
@@ -15,11 +18,11 @@ class Parameter:
 
 class Error:
     def __init__(
-        self,
-        code: str,
-        message: str,
-        mitigations: Union[str, Iterable[str]],
-        parameters: Dict[str, Union[str, Parameter]],
+            self,
+            code: str,
+            message: str,
+            mitigations: Union[str, Iterable[str]],
+            parameters: Dict[str, Union[str, Parameter]],
     ):
         # This function maybe flattened into or moved out of the constructor in the future.
         def build_error(code, msg, mitigations, params):
@@ -47,11 +50,50 @@ class Error:
     # See also see, Github Issue #28 https://github.com/exasol/error-reporting-python/issues/28
 
 
+# ATTENTION: In the event of an exception while creating an error, we encounter a chicken-and-egg problem regarding error definitions.
+# Therefore, errors created by this library must be defined "statically" within this file.
+# Details should then be used to create a low-level error. The information should also be used to create/update the error-codes.json
+# as part of the release preparation. This is only necessary for this library, as it is the root, other libraries should use
+# the `ec` command-line tool to update and create their project specifc error-codes.json file.
+LIBRARY_ERRORS = {
+    "E-ERP-1": {
+        "identifier": "E-ERP-1",
+        "message": "Invalid error code {{code}}.",
+        "messagePlaceholders": [
+            {
+                "placeholder": "code",
+                "description": "Error code which was causing the error."
+            }
+        ],
+        "mitigations": ["Ensure you follow the standard error code format."],
+        "sourceFile": Path(__file__).name
+    },
+    "E-ERP-2": {
+        "identifier": "E-ERP-2",
+        "message": "Unknown error/exception occurred.",
+        "messagePlaceholders": [
+            {
+                "placeholder": "traceback",
+                "description": "Exception traceback which lead to the generation of this error."
+            }
+        ],
+        "description": "An unexpected error occurred during the creation of the error",
+        "mitigations": [cleandoc("""
+                    A good starting point would be to investigate the cause of the attached exception.
+        
+                    Trackback:
+                        {{traceback}}
+                    """)],
+        "sourceFile": Path(__file__).name
+    },
+}
+
+
 def ExaError(
-    code: str,
-    message: str,
-    mitigations: Union[str, List[str]],
-    parameters: Mapping[str, Union[str, Parameter]],
+        code: str,
+        message: str,
+        mitigations: Union[str, List[str]],
+        parameters: Mapping[str, Union[str, Parameter]],
 ) -> Error:
     """Create a new ExaError.
 
@@ -66,33 +108,42 @@ def ExaError(
 
     Returns:
         An error type containing all relevant information.
-
-
-    Raises:
-        This function should not raise under any circumstances.
-        In case of error, it should report it also as an error type which is returned by this function.
-        Still the returned error should contain a references or information about the original call.
-
-        Attention:
-
-            FIXME: Due to legacy reasons this function currently still may raise an `ValueError` (Refactoring Required).
-
-            Potential error scenarios which should taken into account are the following ones:
-
-             * E-ERP-1: Invalid error code provided
-                    params:
-                        * Original ErrorCode
-                        * Original error message
-                        * Original mitigations
-                        * Original parameters
-             * E-ERP-2 Unknown error/exception occurred
-                    params:
-                        * exception/msg
-                        * Original ErrorCode
-                        * Original error message
-                        * Original mitigations
-                        * Original parameters
-
-            see also [Github Issue #27](https://github.com/exasol/error-reporting-python/issues/27)
     """
-    return Error(code, message, mitigations, parameters)
+    try:
+        return Error(code, message, mitigations, parameters)
+    except InvalidErrorCode:
+        error_code = 'E-ERP-1'
+        error_details = LIBRARY_ERRORS[error_code]
+        return Error(
+            code=error_details['identifier'],
+            message=error_details['message'],
+            mitigations=error_details['mitigations'],
+            parameters={"code": code}
+        )
+    except Exception as ex:
+        import traceback
+        tb = traceback.format_exc()
+        parameters = {"traceback": tb}
+        error_code = 'E-ERP-2'
+        error_details = LIBRARY_ERRORS[error_code]
+        return Error(
+            code=error_details['identifier'],
+            message=error_details['message'],
+            mitigations=error_details['mitigations'],
+            parameters=parameters
+        )
+
+
+def _create_error_code_definitions(version=None):
+    from exasol.error.version import VERSION
+    version = version or VERSION
+    return {
+        "$schema": "https://schemas.exasol.com/error_code_report-1.0.0.json",
+        "projectName": "exasol-error-reporting",
+        "projectVersion": version,
+        "errorCodes": [code for code in LIBRARY_ERRORS.values()]
+    }
+
+
+if __name__ == "__main__":
+    pass
--- noxconfig.py
+++ noxconfig.py
@@ -5,6 +5,24 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterable
 
+from exasol.toolbox.nox.plugin import hookimpl
+
+
+class UpdateErrorCodes:
+    ERROR_CODES: Path = Path(__file__).parent / "error-codes.json"
+
+    @hookimpl
+    def prepare_release_update_version(self, session, config, version):
+        import json
+        from exasol.error._error import _create_error_code_definitions
+        definitions = _create_error_code_definitions(f'{version}')
+        with open(UpdateErrorCodes.ERROR_CODES, 'w') as f:
+            json.dump(definitions, f)
+
+    @hookimpl
+    def prepare_release_add_files(self, session, config):
+        return [self.ERROR_CODES]
+
 
 @dataclass(frozen=True)
 class Config:
@@ -14,7 +32,7 @@ class Config:
     doc: Path = Path(__file__).parent / "doc"
     version_file: Path = Path(__file__).parent / "exasol" / "error" / "version.py"
     path_filters: Iterable[str] = ("dist", ".eggs", "venv")
-    plugins = []
+    plugins = [UpdateErrorCodes]
 
 
 PROJECT_CONFIG = Config()
